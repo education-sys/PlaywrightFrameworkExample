b8cf6e8991760c60abd7b4e669b3e4a3bb4b62c1
QA academy <education@qaacademy.rs>
2026-01-28 18:55:16 +0100

    Removed buyflow

--- tests/mainFlows.spec.ts
+++ tests/mainFlows.spec.ts
@@ -1,11 +1,2 @@
 import { test, expect } from './BaseTest';
 
-test('main buyflow', async ({ loginPage, homePage, checkoutStepTwo, checkoutStepOne, checkoutComplete, cartPage }) => {
-    await loginPage.login('standard_user', 'secret_sauce');
-    await loginPage.assertLoggedIn();
-    await homePage.addToCartAction();
-    await cartPage.clickOnCheckOutButton();
-    await checkoutStepOne.addToCartAction();
-    await checkoutStepTwo.clickOnFinishButton();
-    await checkoutComplete.orderConfirmationHeadlinePresent();
-  });
